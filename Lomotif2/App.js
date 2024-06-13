fceeebf3603b802b8963d3b4d6be586c62688173
gabriel camara <gabricamara31@gmail.com>
2024-06-13 03:21:10 -0300

    atualiza o primeiro aplicativo

--- Lomotif2/App.js
+++ Lomotif2/App.js
@@ -1,19 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View} from "react-native";
 
+import * as ImagePicker from 'expo-image-picker';
 import Button from './components/Button'; 
 import ImageViewer from './components/ImageViewer';
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
+      <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
@@ -36,4 +49,3 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
-
